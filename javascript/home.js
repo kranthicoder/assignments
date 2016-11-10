d99ea2d79ae370e0fd0ebb6a4b68abfb96c42f2f
Kranthi Kumar <kranthi.coder@gmail.com>
2016-11-10 01:08:38 -0600

    basics of javascript

---
+++ javascript/home.js
@@ -0,0 +1,10 @@
+function myFunction() {
+   document.getElementById("div10").innerHTML = "<b>" +
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+    tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam, \
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconseq \
+    uat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolor \
+    e eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt \
+    in culpa qui officia deserunt mollit anim id est laborum."; + "</b>"
+
+}
